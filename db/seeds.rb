f640fde2be275281a5546aa532a68a0fde82e826
Theo Chatonnet <theo.chatonnet@gmail.com>
2023-08-16 14:52:57 +0200

    RAILS YELP MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '1 Quai de Bacalan, 33300 Bordeaux', phone_number: '07 88 54 52 09', category: 'chinese' }
+bistrot_maurice = { name: 'Bistrot Maurice', address: '2 Quai de Bacalan, 33300 Bordeaux', phone_number: '06 88 54 52 09', category: 'french' }
+le_confidentiel = { name: 'Le Confidentiel', address: '80 Quai des Chartrons, 33300 Bordeaux', phone_number: '07 89 54 52 90', category: 'belgian' }
+le_bordeaux = { name: 'Le Bordeaux', address: 'Place de la comedy', phone_number: '05 57 30 43 46', category: 'italian' }
+bistrot_des_anges = { name: 'Le bistrot des Anges', address: '19 Rue Rode, 33000 Bordeaux', phone_number: '05 88 54 52 09', category: 'french' }
+
+[dishoom, bistrot_maurice, le_confidentiel, le_bordeaux, bistrot_des_anges].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
